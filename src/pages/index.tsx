96aa1fcc158d9f23337b9f64a84cc88ea0c71e76
Egge <egge@getcurrent.io>
2024-12-10 14:38:07 +0100

    fixed paths

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import useBaseUrl from "@docusaurus/useBaseUrl";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
 
 export default function Home(): JSX.Element {
   const { siteConfig } = useDocusaurusContext();
+  const docsLink = useBaseUrl("docs/Introduction/getting_started");
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
@@ -12,7 +14,10 @@ export default function Home(): JSX.Element {
         <div className="flex flex-col items-center p-8 ">
           <div className="py-32 flex flex-col items-center justify-center text-center gap-8">
             <div className="flex gap-4">
-              <img src={"/img/tslogo.png"} className="w-12 h-12" />
+              <img
+                src={require("@site/static/img/tslogo.png").default}
+                className="w-12 h-12"
+              />
               <h1 className="text-5xl font-extrabold max-w-5xl text-center">
                 cashu-ts
               </h1>
@@ -27,7 +32,7 @@ export default function Home(): JSX.Element {
             <div className="flex gap-4">
               <a
                 className="px-4 py-2 bg-purple-900 hover:bg-purple-700 rounded-full border-none text-white hover:no-underline hover:text-white"
-                href="/docs/Introduction/getting_started"
+                href={docsLink}
               >
                 Getting Started
               </a>
